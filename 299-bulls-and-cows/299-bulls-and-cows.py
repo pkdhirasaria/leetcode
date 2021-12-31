a956c1cfa9b15560e5e1f7d7d7379dece0055b3e
Piyush Agarwal <pkdhirasaria@gmail.com>
2021-12-31 10:51:14 +0530

    [ Hash Table, String, Counting ] Medium

--- 299-bulls-and-cows/299-bulls-and-cows.py
+++ 299-bulls-and-cows/299-bulls-and-cows.py
@@ -1,11 +1,7 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
         x = 0
-        y = 0
-        for a,b in zip(secret,guess):
-            if a== b:
-                x += 1
-                
+        y = 0   
         c = defaultdict(int)
         for i in range(len(secret)):
             if secret[i] != guess[i]:
@@ -15,5 +11,7 @@ class Solution:
             if c.get(guess[i],0) > 0 and guess[i] != secret[i]:
                 y += 1
                 c[guess[i]] -=1
+            if guess[i] == secret[i]:
+                x += 1
         return str(x) + "A" + str(y) + "B"
                 
