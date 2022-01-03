eaba7d6b15d824decaa8c14b0b12cc11d4113c65
Piyush Agarwal <pkdhirasaria@gmail.com>
2022-01-03 19:48:41 +0530

    [ String ] Easy

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -1,7 +1,3 @@
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        count =0
-        for p in patterns:
-            if p in word:
-                count += 1
-        return count
+         return sum(p in word for p in patterns)
