c3539df92436e453b4cde4dba8baea850ada8ac4
Piyush Agarwal <pkdhirasaria@gmail.com>
2022-01-06 00:49:47 +0530

    [ Hash Table, Tree, Depth-First Search, Breadth-First Search, Binary Tree ] Medium

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode], d= 0) -> Optional[TreeNode]:
+        self.depth = 0
+        self.lca = None
+        # print(root.val,d)
+        def dfs(root,d):
+            
+            self.depth = max(self.depth,d)
+            if not root:
+                return d
+            
+            left = dfs(root.left,d+1) 
+            right = dfs(root.right,d+1) 
+            if left == right == self.depth:
+                self.lca = root
+                self.depth = left
+            print(root.val,d,self.depth,self.lca if not self.lca else self.lca.val)
+            return max(left,right) 
+        dfs(root,0)
+        return self.lca
