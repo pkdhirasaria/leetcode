6765bec12802c95f32d46df678cf5ad7816d2a9a
Piyush Agarwal <pkdhirasaria@gmail.com>
2022-01-05 11:33:03 +0530

    [ Tree, Depth-First Search, Binary Tree ] Medium

--- 437-path-sum-iii/437-path-sum-iii.py
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -6,20 +6,38 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        prefix = defaultdict(int)
-        prefix[0] = 1
-        self.res = 0
-        def dfs(root,targetSum,prev_sum):
+        def dfs(root,lookup):
             if not root:
-                return
-            prev_sum += root.val
-            self.res += prefix[prev_sum-targetSum]
-            prefix[prev_sum] += 1
-            
-            dfs(root.left,targetSum,prev_sum)
-            dfs(root.right,targetSum,prev_sum)
-            
-            prefix[prev_sum] -= 1
-        dfs(root,targetSum,0)    
+                return []
+            lookup[root] = [root.val]
+            for val in dfs(root.left,lookup):
+                lookup[root].append(root.val + val)
+            for val in dfs(root.right,lookup):
+                lookup[root].append(root.val + val)    
+            return lookup[root]
+        lookup = {}
+        count =0
+        dfs(root,lookup)
+        for l in lookup:
+            for val in lookup[l]:
+                if val == targetSum:
+                    count += 1
+        return count
                 
-        return self.res
+#         prefix = defaultdict(int)
+#         prefix[0] = 1
+#         self.res = 0
+#         def dfs(root,targetSum,prev_sum):
+#             if not root:
+#                 return
+#             prev_sum += root.val
+#             self.res += prefix[prev_sum-targetSum]
+#             prefix[prev_sum] += 1
+            
+#             dfs(root.left,targetSum,prev_sum)
+#             dfs(root.right,targetSum,prev_sum)
+            
+#             prefix[prev_sum] -= 1
+#         dfs(root,targetSum,0)    
+                
+        # return self.res
