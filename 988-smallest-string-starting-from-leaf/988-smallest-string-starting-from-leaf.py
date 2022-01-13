9de55ae82b7c875eee5fad47f76fd17b45ec9a99
Piyush Agarwal <pkdhirasaria@gmail.com>
2022-01-13 23:24:21 +0530

    [ String, Tree, Depth-First Search, Binary Tree ] Medium

---
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
+        q = deque()
+        q.append((root,chr(root.val+97)))
+        ans = 'z'*9000
+        while q:
+            node,val = q.popleft()
+            # print(node.val,val)
+            if (node.left,node.right) == (None,None): 
+                ans = min(ans,val)
+            if node.left:
+                q.append((node.left,chr(node.left.val + 97) + val))
+            if node.right:
+                q.append((node.right,chr(node.right.val + 97) + val))
+        return ans
+        
+        # def dfs(root):
+        #     if (root.left,root.right) == (None,None):
+        #         return chr(root.val + 97)
+        #     left,right = '',''
+        #     if root.left:
+        #         left = dfs(root.left)
+        #     if root.right:
+        #         right = dfs(root.right)
+        #     r = chr(root.val + 97)
+        #     print(left,right,left+r,right+r)
+        #     if left+r < right + r:
+        #         return left + r
+        #     else:
+        #         return right + r
+        # return dfs(root)
