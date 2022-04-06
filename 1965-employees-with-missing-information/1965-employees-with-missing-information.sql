7d53deb8065a6cdcd879363199ca15e7b8bbef31
Piyush Agarwal <pkdhirasaria@gmail.com>
2022-04-06 14:40:13 +0530

    [ Database ] Easy

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,8 +1,4 @@
-# Write your MySQL query statement below
-select COALESCE(Emp_id,S_id) as employee_id
-from 
-((select e.employee_id as Emp_id, e.name, s.employee_id as S_id,s.salary from Employees e left join Salaries s on e.employee_id =s.employee_id )
-union 
-(select e.employee_id as Emp_id, e.name, s.employee_id as S_id,s.salary from Employees e right join Salaries s on e.employee_id =s.employee_id )) a
-where a.name is null or a.salary is null
+/* Write your T-SQL query statement below */
+select COALESCE(e.employee_id,s.employee_id) as employee_id from Employees e full outer join Salaries s on e.employee_id = s.employee_id
+where name is null or salary is null
 order by 1
