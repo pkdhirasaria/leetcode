79a27a45805474fc6c55f2dfc8a7d5728d28f81f
Piyush Agarwal <pkdhirasaria@gmail.com>
2022-04-22 01:57:28 +0530

    [ Backtracking, Depth-First Search, Breadth-First Search, Graph ] Medium

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -5,17 +5,16 @@ class Solution:
         def dfs(node,path):
             if node == n-1:
                 # print(path)
-                self.ans.append(path.copy())
+                self.ans.append(path)
                 # print(self.ans)
                 return
             for g in graph[node]:
-                path.append(g)
+                # path.append(g)
                 # print(node,g,path)
-                dfs(g,path)
-                path.pop()
+                dfs(g,path+[g])
+                # path.pop()
                 # print(node,g,path)
             return
-        path = []
-        path.append(0)
-        dfs(0,path)
+    
+        dfs(0,[0])
         return self.ans
